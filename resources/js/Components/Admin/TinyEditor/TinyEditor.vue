09ab180ccd69005f19c9b7efeae8ad02e02ad735
Александр Косолапов <kosolapov1976@gmail.com>
2025-05-03 12:22:37 +0500

    fix: TinyEditor.vue

--- resources/js/Components/Admin/TinyEditor/TinyEditor.vue
+++ resources/js/Components/Admin/TinyEditor/TinyEditor.vue
@@ -185,32 +185,38 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <div class="flex flex-row justify-between">
-        <!-- Связываем value напрямую с props.modelValue -->
-        <textarea :id="editorId" :value="props.modelValue"></textarea>
+    <div class="flex flex-row justify-between items-start"> <!-- Добавил items-start для выравнивания, если нужно -->
+        <!-- Текстовое поле редактора -->
+        <div class="flex-grow"> <!-- Обертка, чтобы текстовое поле занимало доступное место -->
+            <textarea :id="editorId" :value="props.modelValue"></textarea>
+        </div>
 
-        <button @click="toggleTheme" :title="t('changeEditorTheme')"
-                class="flex items-center justify-center
-                       ml-2 w-6 h-6 rounded-full
-                       bg-slate-100 dark:bg-slate-900 hover:bg-yellow-100 dark:hover:bg-slate-600/80
-                       transition-colors">
-            <!-- SVG иконки -->
-            <svg v-if="!isDarkTheme"
-                 xmlns="http://www.w3.org/2000/svg"
-                 class="w-4 h-4" width="16" height="16">
-                <path class="fill-current text-red-400 dark:text-yellow-200"
-                      d="M7 0h2v2H7V0Zm5.88 1.637 1.414 1.415-1.415 1.413-1.414-1.414 1.415-1.414ZM14 7h2v2h-2V7Zm-1.05 7.433-1.415-1.414 1.414-1.414 1.415 1.413-1.414 1.415ZM7 14h2v2H7v-2Zm-4.02.363L1.566 12.95l1.415-1.414 1.414 1.415-1.415 1.413ZM0 7h2v2H0V7Zm3.05-5.293L4.465 3.12 3.05 4.535 1.636 3.121 3.05 1.707Z"/>
-                <path class="fill-current text-red-400 dark:text-yellow-200"
-                      d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"/>
-            </svg>
-            <svg v-else
-                 xmlns="http://www.w3.org/2000/svg"
-                 class="w-4 h-4" width="16" height="16">
-                <path class="fill-current text-slate-900 dark:text-white"
-                      d="M6.2 2C3.2 2.8 1 5.6 1 8.9 1 12.8 4.2 16 8.1 16c3.3 0 6-2.2 6.9-5.2C9.7 12.2 4.8 7.3 6.2 2Z"/>
-                <path class="fill-current text-slate-900 dark:text-white"
-                      d="M12.5 6a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 6Z"/>
-            </svg>
+        <!-- Кнопка переключения темы -->
+        <button
+            type="button"
+        @click="toggleTheme"
+        :title="t('changeEditorTheme')"
+        class="flex items-center justify-center
+        ml-2 w-6 h-6 flex-shrink-0 rounded-full <!-- flex-shrink-0 чтобы кнопка не сжималась -->
+        bg-slate-100 dark:bg-slate-900 hover:bg-yellow-100 dark:hover:bg-slate-600/80
+        transition-colors">
+        <!-- SVG иконки -->
+        <svg v-if="!isDarkTheme"
+             xmlns="http://www.w3.org/2000/svg"
+             class="w-4 h-4" width="16" height="16">
+            <path class="fill-current text-red-400 dark:text-yellow-200"
+                  d="M7 0h2v2H7V0Zm5.88 1.637 1.414 1.415-1.415 1.413-1.414-1.414 1.415-1.414ZM14 7h2v2h-2V7Zm-1.05 7.433-1.415-1.414 1.414-1.414 1.415 1.413-1.414 1.415ZM7 14h2v2H7v-2Zm-4.02.363L1.566 12.95l1.415-1.414 1.414 1.415-1.415 1.413ZM0 7h2v2H0V7Zm3.05-5.293L4.465 3.12 3.05 4.535 1.636 3.121 3.05 1.707Z"/>
+            <path class="fill-current text-red-400 dark:text-yellow-200"
+                  d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"/>
+        </svg>
+        <svg v-else
+             xmlns="http://www.w3.org/2000/svg"
+             class="w-4 h-4" width="16" height="16">
+            <path class="fill-current text-slate-900 dark:text-white"
+                  d="M6.2 2C3.2 2.8 1 5.6 1 8.9 1 12.8 4.2 16 8.1 16c3.3 0 6-2.2 6.9-5.2C9.7 12.2 4.8 7.3 6.2 2Z"/>
+            <path class="fill-current text-slate-900 dark:text-white"
+                  d="M12.5 6a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 6Z"/>
+        </svg>
         </button>
     </div>
 </template>
